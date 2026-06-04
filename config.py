7cd3b01a21dfcfea9ae0042ef1f00534272875c5
sodonbud <sodonbud@gmail.com>
2026-06-04 12:00:14 +0800

    update 1

--- config.py
+++ config.py
@@ -1,11 +1,11 @@
 DEVICES = {
-    "Phone1": "SERIAL_A",
+    "Phone1": "RF8Y606FYXX",
     "Phone2": "SERIAL_B",
 }
 
 EXCEL_PATH = "test_cases.xlsx"
 SHEET_NAME = "TestCases"
-ADB_PATH = "adb"  # or full path if not in PATH, e.g. "/usr/local/bin/adb"
+ADB_PATH = "C:/Users/DELL/Documents/GitHub/platform-tools/adb.exe"  # or full path if not in PATH, e.g. "/usr/local/bin/adb"
 
 ADB_TIMEOUT = 30  # seconds per ADB command
 
